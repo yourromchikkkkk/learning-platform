4a3e3ea8c93895b1e6daf8e6e50a915a71f452be
Roman Matsueiv <romanmatsuiev@gmail.com>
2023-09-28 12:36:41 +0300

    init

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@/components/ui/button';
+
+export default function Home() {
+  return (
+    <div>
+      <p className="text-3xl font-medium text-slate-500">Hello World!</p>
+      <Button variant="destructive">Click me!</Button>
+    </div>
+  );
+}
