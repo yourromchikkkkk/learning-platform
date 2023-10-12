0dde06f483ba766cf6b1064bb41c43c565780d44
Roman Matsuiev <romanmatsuiev@gmail.com>
2023-10-12 16:02:37 +0300

    feat: add course create endpoint

---
+++ app/api/courses/routes.ts
@@ -0,0 +1,33 @@
+import {
+  NextRequest,
+  NextResponse,
+} from 'next/server';
+import { auth } from '@clerk/nextjs';
+import { db } from '@/lib/db';
+
+export async function POST(request: NextRequest) {
+  try {
+    const { userId } = auth();
+    const { title } = await request.json();
+
+    if (!userId) {
+      return new NextResponse('Unauthorized', {
+        status: 401,
+      });
+    }
+
+    const course = await db.course.create({
+      data: {
+        userId,
+        title,
+      },
+    });
+
+    return NextResponse.json(course);
+  } catch (error) {
+    console.error('[COURSES]:', error);
+    return new NextResponse('Internal Error', {
+      status: 500,
+    });
+  }
+}
