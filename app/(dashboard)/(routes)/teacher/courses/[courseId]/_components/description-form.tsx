8b31ac0b3dc01549f82ac49e4c7a9e2a2af902ce
Roman Matsuiev <romanmatsuiev@gmail.com>
2023-11-02 13:39:36 +0200

    feat: add field to edit course description

---
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/description-form.tsx
@@ -0,0 +1,133 @@
+'use client';
+
+import { useCallback, useState } from 'react';
+import * as z from 'zod';
+import axios from 'axios';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from '@/components/ui/form';
+import { Button } from '@/components/ui/button';
+import { PencilIcon } from 'lucide-react';
+import toast from 'react-hot-toast';
+import { useRouter } from 'next/navigation';
+import { cn } from '@/lib/utils';
+import { Textarea } from '@/components/ui/textarea';
+
+interface IDescriptionForm {
+  initialData: {
+    description: string | null;
+  };
+  courseId: string;
+}
+
+const formSchema = z.object({
+  description: z.string().min(1, {
+    message: 'Description is required',
+  }),
+});
+
+const DescriptionForm: React.FC<IDescriptionForm> = ({
+  initialData,
+  courseId,
+}) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const router = useRouter();
+
+  const toggleEdit = useCallback(
+    () => setIsEditing((prev) => !prev),
+    [],
+  );
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      description: initialData.description || '',
+    },
+  });
+
+  const { isSubmitting, isValid } = form.formState;
+
+  const onSubmit = async (
+    value: z.infer<typeof formSchema>,
+  ) => {
+    try {
+      await axios.patch(`/api/courses/${courseId}`, value);
+      toggleEdit();
+      toast.success('Course updated');
+      router.refresh();
+    } catch (error) {
+      console.error('error', error);
+      toast.error('Somthing went wrong');
+    }
+  };
+
+  return (
+    <div className="mt-6 border bg-slate-100 rounded-md p-4">
+      <div className="font-medium flex items-center justify-between">
+        Course Description
+        <Button onClick={toggleEdit} variant="ghost">
+          {isEditing ? (
+            <>Cancel</>
+          ) : (
+            <>
+              <PencilIcon className="h-4 w-4 mr-2" />
+              Edit description
+            </>
+          )}
+        </Button>
+      </div>
+      {!isEditing && (
+        <p
+          className={cn('text-sm mt-2', {
+            ['text-slate-500 italic']:
+              !initialData.description,
+          })}
+        >
+          {initialData.description || 'No description'}
+        </p>
+      )}
+      {isEditing && (
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="space-y-4 mt-4"
+          >
+            <FormField
+              control={form.control}
+              name="description"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Textarea
+                      disabled={isSubmitting}
+                      placeholder='e.g. "This course is about..."'
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <div className="flex items-center gap-x-2">
+              <Button
+                disabled={!isValid || isSubmitting}
+                type="submit"
+              >
+                Save
+              </Button>
+            </div>
+          </form>
+        </Form>
+      )}
+    </div>
+  );
+};
+
+export default DescriptionForm;
--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -5,6 +5,7 @@ import { auth } from '@clerk/nextjs';
 import { LayoutDashboardIcon } from 'lucide-react';
 import { redirect } from 'next/navigation';
 import TitleForm from './_components/title-form';
+import DescriptionForm from './_components/description-form';
 
 const CourseDataPage = async ({
   params,
@@ -56,6 +57,10 @@ const CourseDataPage = async ({
             </h2>
           </div>
           <TitleForm
+            initialData={course}
+            courseId={course.id}
+          />
+          <DescriptionForm
             initialData={course}
             courseId={course.id}
           />
